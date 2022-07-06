45bce99a06f30ed29ed01f8fb4c19103f03522ef
Dhruv Bhardwaj <96115027+Dhruv-bhardwaj99@users.noreply.github.com>
2022-07-06 16:17:05 +0530

    backend

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () =>{
-    return mongoose.connect("mongodb+srv://dhruv:dhruv12@cluster0.dcpqvb8.mongodb.net/times?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://dhruv:dhruv12@cluster0.iecuagt.mongodb.net/pro")
 }
